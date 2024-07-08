d84b738cd48025b0e1a1cd2d1339f233abe1a676
Zuhrul Anam <zuhrrlanam@gmail.com>
2024-07-08 20:56:16 +0700

    Initial Commit

---
+++ ArticleAppIos/ArticleAppIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleAppIosApp.swift
+//  ArticleAppIos
+//
+//  Created by WDT on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ArticleAppIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
